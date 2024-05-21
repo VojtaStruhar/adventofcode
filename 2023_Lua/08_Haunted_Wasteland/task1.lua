3c1e1c2a8cbaf2e573e9d5e0e74b9758aac20631
Vojtěch Struhár <vojtechstruhar@gmail.com>
2024-05-21 17:32:01 +0200

    Refactor Day 8, Task 1

--- 2023_Lua/08_Haunted_Wasteland/task1.lua
+++ 2023_Lua/08_Haunted_Wasteland/task1.lua
@@ -1,30 +1,9 @@
-local f = io.open("input.txt")
-if f == nil then
-    print("FAILED TO OPEN FILE")
-    return
+local utils = require("utils")
+local task = utils.load_task("input.txt")
+
+if not task then
+	return
 end
-
-
-local moves = f:read("l")
-print("Moves sequence: " .. tostring(string.len(moves)))
-
-local _ = f:read("l") -- skip one line
-
-local map = {}
-
-local line = f:read("l")
-while line ~= nil
-do
-    local node = string.sub(line, 1, 3)
-
-    map[node] = {
-        L = string.sub(line, 8, 10),
-        R = string.sub(line, 13, 15)
-    }
-
-    line = f:read("l") -- advance
-end
-
 
 local start = "AAA"
 local goal = "ZZZ"
@@ -34,20 +13,20 @@ local iterations = 0
 local max_iterations = 1000000 -- 1 million
 
 while current ~= goal do
-    if iterations >= max_iterations then
-        print("MAX ITERATIONS EXCEEDED: " .. tostring(max_iterations))
-        break
-    end
+	if iterations >= max_iterations then
+		print("MAX ITERATIONS EXCEEDED: " .. tostring(max_iterations))
+		break
+	end
 
-    local LR = string.sub(moves, stepIndex, stepIndex)
-    current = map[current][LR]
-    -- print("progress:", LR, stepIndex, "->", current)
-    iterations = iterations + 1
-    if current == goal then
-        print("Reached " .. goal .. " in " .. tostring(iterations) .. " iterations!")
-        break
-    end
+	local LR = string.sub(task.moves, stepIndex, stepIndex)
+	current = task.map[current][LR]
+	-- print("progress:", LR, stepIndex, "->", current)
+	iterations = iterations + 1
+	if current == goal then
+		print("Reached " .. goal .. " in " .. tostring(iterations) .. " iterations!")
+		break
+	end
 
-    -- move along
-    stepIndex = stepIndex % string.len(moves) + 1
+	-- move along
+	stepIndex = stepIndex % string.len(task.moves) + 1
 end
---
+++ 2023_Lua/08_Haunted_Wasteland/utils.lua
@@ -0,0 +1,32 @@
+M = {}
+
+function M.load_task(filename)
+	local f = io.open(filename)
+	if f == nil then
+		print("FAILED TO OPEN FILE: " .. filename)
+		return
+	end
+
+	local result = {
+		map = {},
+		moves = f:read("l"),
+	}
+
+	local _ = f:read("l") -- skip one line
+
+	local line = f:read("l")
+	while line ~= nil do
+		local node = string.sub(line, 1, 3)
+
+		result.map[node] = {
+			L = string.sub(line, 8, 10),
+			R = string.sub(line, 13, 15),
+		}
+
+		line = f:read("l") -- advance
+	end
+
+	return result
+end
+
+return M
