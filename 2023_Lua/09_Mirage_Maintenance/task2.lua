d03f1c09ec557377c9b54e33cba117688a2dbe54
Vojtěch Struhár <vojtechstruhar@gmail.com>
2024-05-21 20:45:07 +0200

    Day 9, Task 2

--- 2023_Lua/09_Mirage_Maintenance/task2.lua
+++ 2023_Lua/09_Mirage_Maintenance/task2.lua
@@ -1,6 +1,5 @@
 local filename = arg[1] or "input.txt"
 local utils = require("utils")
-local last = utils.last
 
 local total = 0
 
@@ -12,16 +11,17 @@ for line in utils.lines_iterator(filename) do
 
     local history = utils.create_history(current_row)
 
-    -- extrapolate
-    table.insert(last(history), 0)
+    -- kinda prepend 0
+    table.insert(utils.last(history), 1, 0)
+
+    -- extrapolate, going from top to (original) base
     for i = #history, 2, -1 do
-        local extr_value = last(history[i - 1]) + last(history[i])
-        table.insert(history[i - 1], extr_value)
+        local extr_value = history[i - 1][1] - history[i][1]
+        table.insert(history[i - 1], 1, extr_value)
     end
 
     -- update score
-    total = total + last(history[1])
+    total = total + history[1][1]
 end
-
 
 print("Result:", total)
